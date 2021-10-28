aaeea9b51fa54cb967cc371e72720585498a1e2f
Abraão Borges <abraao_borges@hotmail.com>
2021-10-28 07:30:45 -0300

    H2 database

--- backend/src/main/java/com/absoftware/dsclient/resources/ClientResource.java
+++ backend/src/main/java/com/absoftware/dsclient/resources/ClientResource.java
@@ -1,25 +1,26 @@
 package com.absoftware.dsclient.resources;
 
-import java.time.Instant;
-import java.util.ArrayList;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.absoftware.dsclient.entities.Client;
+import com.absoftware.dsclient.services.ClientService;
 
 @RestController
 @RequestMapping(value = "/clients")
 public class ClientResource {
 	
+	@Autowired
+	private ClientService service;
+	
 	@GetMapping
 	public ResponseEntity<List<Client>> findAll(){
-		List<Client> list = new ArrayList<>();
-		list.add(new Client(1L, "Maria", "405.788.125-89", 1200.3, Instant.now(), 2 ));
-		list.add(new Client(2L, "Augusto", "455.338.135-89", 1500.5, Instant.now(), 1 ));
+		List<Client> list = service.findAll();
 		return ResponseEntity.ok().body(list);
 	}
 
---
+++ backend/src/main/java/com/absoftware/dsclient/services/ClientService.java
@@ -0,0 +1,21 @@
+package com.absoftware.dsclient.services;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.absoftware.dsclient.entities.Client;
+import com.absoftware.dsclient.repositories.ClientRepository;
+
+@Service
+public class ClientService {
+
+	@Autowired
+	private ClientRepository repository;
+	
+	public List<Client> findAll(){
+		return repository.findAll();
+	}
+	
+}
