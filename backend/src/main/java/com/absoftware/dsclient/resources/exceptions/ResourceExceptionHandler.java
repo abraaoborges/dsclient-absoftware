b80e317fa033db117a5bb0dd0154846387227ab3
Abraão Borges <abraao_borges@hotmail.com>
2021-10-30 09:19:54 -0300

    Update Client

--- backend/src/main/java/com/absoftware/dsclient/resources/exceptions/ResourceExceptionHandler.java
+++ backend/src/main/java/com/absoftware/dsclient/resources/exceptions/ResourceExceptionHandler.java
@@ -9,19 +9,14 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
-import com.absoftware.dsclient.services.exceptions.EntityNotFoundException;
+import com.absoftware.dsclient.services.exceptions.DatabaseException;
+import com.absoftware.dsclient.services.exceptions.ResourceNotFoundException;
 
 @ControllerAdvice
 public class ResourceExceptionHandler {
 
-	/*
-	 * @ExceptionHandler(ResourceNotFoundException.class) public
-	 * ResponseEntity<StandardError> entityNotFound(ResourceNotFoundException e,
-	 * HttpServletRequest request){
-	 */
-	
-	@ExceptionHandler(EntityNotFoundException.class)
-	public ResponseEntity<StandardError> entityNotFound(EntityNotFoundException e, HttpServletRequest request) {
+	@ExceptionHandler(ResourceNotFoundException.class)
+	public ResponseEntity<StandardError> entityNotFound(ResourceNotFoundException e, HttpServletRequest request) {
 		HttpStatus status = HttpStatus.NOT_FOUND;
 		StandardError err = new StandardError();
 		err.setTimestamp(Instant.now());
@@ -34,17 +29,18 @@ public class ResourceExceptionHandler {
 
 	}
 
-	/*
-	 * @ExceptionHandler(DatabaseException.class) public
-	 * ResponseEntity<StandardError> database(DatabaseException e,
-	 * HttpServletRequest request){ HttpStatus status = HttpStatus.BAD_REQUEST;
-	 * StandardError err = new StandardError(); err.setTimestamp(Instant.now());
-	 * err.setStatus(status.value()); err.setError("Database exception");
-	 * err.setMessage(e.getMessage()); err.setPath(request.getRequestURI());
-	 * 
-	 * return ResponseEntity.status(status).body(err);
-	 * 
-	 * }
-	 */
+	@ExceptionHandler(DatabaseException.class)
+	public ResponseEntity<StandardError> database(DatabaseException e, HttpServletRequest request) {
+		HttpStatus status = HttpStatus.BAD_REQUEST;
+		StandardError err = new StandardError();
+		err.setTimestamp(Instant.now());
+		err.setStatus(status.value());
+		err.setError("Database exception");
+		err.setMessage(e.getMessage());
+		err.setPath(request.getRequestURI());
+
+		return ResponseEntity.status(status).body(err);
+
+	}
 
 }
--- backend/src/main/java/com/absoftware/dsclient/services/ClientService.java
+++ backend/src/main/java/com/absoftware/dsclient/services/ClientService.java
@@ -1,9 +1,10 @@
 package com.absoftware.dsclient.services;
 
-import java.time.Instant;
 import java.util.List;
 import java.util.Optional;
 import java.util.stream.Collectors;
+
+import javax.persistence.EntityNotFoundException;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -12,27 +13,28 @@ import org.springframework.transaction.annotation.Transactional;
 import com.absoftware.dsclient.dto.ClientDTO;
 import com.absoftware.dsclient.entities.Client;
 import com.absoftware.dsclient.repositories.ClientRepository;
-import com.absoftware.dsclient.services.exceptions.EntityNotFoundException;
+import com.absoftware.dsclient.services.exceptions.ResourceNotFoundException;
 
 @Service
 public class ClientService {
 
 	@Autowired
 	private ClientRepository repository;
-	
+
 	@Transactional(readOnly = true)
-	public List<ClientDTO> findAll(){
+	public List<ClientDTO> findAll() {
 		List<Client> list = repository.findAll();
-		return list.stream().map(x -> new ClientDTO(x)).collect(Collectors.toList());		
+		return list.stream().map(x -> new ClientDTO(x)).collect(Collectors.toList());
 	}
 
 	@Transactional(readOnly = true)
 	public ClientDTO findById(Long id) {
 		Optional<Client> obj = repository.findById(id);
-		Client entity = obj.orElseThrow(() -> new EntityNotFoundException("Entity not found"));		
+		Client entity = obj.orElseThrow(() -> new ResourceNotFoundException("Entity not found"));
 		return new ClientDTO(entity);
 	}
 
+	@Transactional
 	public ClientDTO insert(ClientDTO dto) {
 		Client entity = new Client();
 		entity.setName(dto.getName());
@@ -40,7 +42,23 @@ public class ClientService {
 		entity.setIncome(dto.getIncome());
 		entity.setBirthDate(dto.getBirthDate());
 		entity.setChildren(dto.getChildren());
-		entity = repository.save(entity);			
-		return new ClientDTO(entity);		
-	}	
+		entity = repository.save(entity);
+		return new ClientDTO(entity);
+	}
+
+	@Transactional
+	public ClientDTO update(Long id, ClientDTO dto) {
+		try {
+			Client entity = repository.getOne(id);
+			entity.setName(dto.getName());
+			entity.setCpf(dto.getCpf());
+			entity.setIncome(dto.getIncome());
+			entity.setBirthDate(dto.getBirthDate());
+			entity.setChildren(dto.getChildren());
+			entity = repository.save(entity);
+			return new ClientDTO(entity);
+		} catch (EntityNotFoundException e) {
+			throw new ResourceNotFoundException("Id not found " + id);
+		}
+	}
 }
